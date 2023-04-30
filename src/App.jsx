bfefd745cd6981d2a1b3a195e62835eabb4a1165
isaiasleibo <isaias.leibovich@gmail.com>
2023-04-29 22:47:39 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -50,6 +50,7 @@ function App() {
                   setIsp(data.connection.isp);
                   setMapLocation([data.latitude, data.longitude]);
                 } else {
+                  setIpReturned("Not found");
                   setLocation("Not found");
                   setTimezone("Not found");
                   setIsp("Not found");
